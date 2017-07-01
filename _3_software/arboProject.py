254b0505ce0dc910c6f8b92afc91935b8222e0bd
poltergeist42 <lepoltergeist@gmail.com>
2017-07-01 20:44:27 +0200

    modif de l'arbo a creer

--- _3_software/arboProject.py
+++ _3_software/arboProject.py
@@ -4,7 +4,7 @@
 """
    :Nom du fichier:     arboProject.py
    :Autheur:            `Poltergeist42 <https://github.com/poltergeist42>`_
-   :Version:            20170330
+   :Version:            20170701
 
 ----
 
@@ -47,21 +47,24 @@ class C_Arbo(object) :
         self.v_chkTrueFalse = True
         # print("dbgMsg[02] : ", self.v_localDir)
         
+        t_lstTxtFile   = (".gitignore", "README.rst", "VoLAB.rst")
+        
         self.l_arboDir =    [
-                            "/_1_userDoc_v",
-                            "/_2_modelization_v",
-                            "/_3_software_v/_3-1_test_v",
-                            "/_3_software_v/oldLibVers",
-                            "/_4_PCB_v",
-                            "/_5_techDoc_v/_5-1_liensWeb_v",
-                            "/_6_research_v/_6-1_Etude_Documentation_v",
-                            "/_6_research_v/_6-2_liensWeb_v",
-                            "/_6_research_v/_6-3_logiciels_v",
-                            "/_7_rushes/_7-1_texts_v",
-                            "/_7_rushes/_7-2_audio_v",
-                            "/_7_rushes/_7-3_video_v",
-                            "/_7_rushes/_7-4_pictures",
-                            "/_7_rushes/_7-5_liensWeb_v"
+                            "/webDoc",
+                            "/project/_1_userDoc_v",
+                            "/project/_2_modelization_v",
+                            "/project/_3_software_v/_3-1_test_v",
+                            "/project/_3_software_v/oldLibVers",
+                            "/project/_4_PCB_v",
+                            "/project/_5_techDoc_v/_5-1_liensWeb_v",
+                            "/project/_6_research_v/_6-1_Etude_Documentation_v",
+                            "/project/_6_research_v/_6-2_liensWeb_v",
+                            "/project/_6_research_v/_6-3_logiciels_v",
+                            "/project/_7_rushes/_7-1_texts_v",
+                            "/project/_7_rushes/_7-2_audio_v",
+                            "/project/_7_rushes/_7-3_video_v",
+                            "/project/_7_rushes/_7-4_pictures",
+                            "/project/_7_rushes/_7-5_liensWeb_v"
                             ]
 
     def f_dirInit(self) :
@@ -87,112 +90,116 @@ class C_Arbo(object) :
                             # N.B : pour windows, les "\\" et '/' seront remplacer
                             # par '\'
 
-    def f_wFile(self, v_fileName) :
+    def f_wFile(self, v_except=None) :
         """ Creation des fichiers textes '.gitignore', 'README.rst' et 'VoLAB.rst' """
-        if v_fileName == ".gitignore" :
-            v_txtData = (
-                        "## [ .gitignore ]\n\n" +
-                        "## Liste des fichiers et dossiers à ignorer\n\n" +
-                        "#.gitignore\n\n" +
-                        "## [ Dossiers a ignorer ]\n" +
-                        "_3_software/_3-1_test*/\n" +
-                        "_3_software/oldLibVers*/\n" +
-                        "_5_*/\n" +
-                        "_6_*/\n" +
-                        "_7_*/\n" +
-                        "*_v/\n\n" +
-                        "## [ listes des extentions a ignorer ]\n" +
-                        "*.FCStd1\n" +
-                        "*.264\n" +
-                        "*.mkv\n" +
-                        "*.mp4\n" +
-                        "*.json\n\n" +
-                        "## Compiled source\n" +
-                        "*.com\n" +
-                        "*.class\n" +
-                        "*.dll\n" +
-                        "*.exe\n" +
-                        "*.o\n" +
-                        "*.so\n" +
-                        "*.pyc\n\n" +
-                        "## Packages\n" +
-                        "*.7z\n" +
-                        "*.dmg\n" +
-                        "*.gz\n" +
-                        "*.iso\n" +
-                        "*.jar\n" +
-                        "*.rar\n" +
-                        "*.tar\n" +
-                        "*.zip\n\n" +
-                        "## Logs and databases\n" +
-                        "*.log\n" +
-                        "*.sql\n" +
-                        "*.sqlite\n\n" +
-                        "## OS generated files\n" +
-                        ".DS_Store?\n" +
-                        "ehthumbs.db\n" +
-                        "Icon?\n" +
-                        "Thumbs.db\n" +
-                        "*.swp\n" +
-                        ".*.swp\n" +
-                        "*~\n" +
-                        "*.lock\n" +
-                        "*.out\n"
-                        )
-                        
-        if v_fileName == "README.rst" :
-            v_txtData = (
-                        "=========================\n" +
-                        "saisir le titre du projet\n" +
-                        "=========================\n\n" +
-                        "   :Autheur:          `Poltergeist42 <https://github.com/poltergeist42>`_\n" +
-                        "   :Projet:          \n" +
-                        "   :Licence:          CC BY-NC-SA 4.0\n" +
-                        "   :Liens:            https://creativecommons.org/licenses/by-nc-sa/4.0/ \n\n" +
-                        "------------------------------------------------------------------------------------------\n\n" +
-                        "Description\n" +
-                        "===========\n\n" +
-                        " Saisir ici une breve description du projet"
-                        )
-                        
-        if v_fileName == "VoLAB.rst" :
-            v_txtData = (
-                        ".. image:: logoVoLAB_200x200.jpg\n" +
-                        "   :align: center\n\n" +
-                        "=====\n" +
-                        "VoLAB\n" +
-                        "=====\n\n" +
-                        "Nous connaître\n" +
-                        "--------------\n\n" +
-                        "   :Le Site Web:       http://www.volab.org \n\n" +
-                        "   :Le Wiki:           http://www.vorobotics.com/wiki/index.php?title=Accueil \n\n" +
-                        "   :GitHub:            https://github.com/volab \n\n" +
-                        "   :Twitter:           https://twitter.com/vorobotics \n\n" +
-                        "   :Faceboock:         https://www.facebook.com/VoLab95/ \n\n" +
-                        "Qui sommes nous ?\n" +
-                        "-----------------\n\n" +
-                        "   Le VoLAB, premier FabLab du Val d'Oise, est un FabLab associatif portée par\n" +
-                        "   l’associassions VoRoBoTics situé à Vauréal (95480).\n\n" +
-                        "   Nous avons pour vocation le partage non marchand des connaissances\n" +
-                        "   et l'échange de compétences. Petits et gros projets se côtoient dans divers domaines comme :\n\n"
-                        "   Le travail du bois, du métal, l'électronique, la programmation, la sérigraphie,\n" +
-                        "   la couture, le scrapbooking et bien d'autres encore.\n\n" +
-                        "   Les échanges dynamiques dans la bonne humeur et le respect mutuel permettent\n" +
-                        "   à chacun d’appendre et de partager à son rythme\n\n" +
-                        "   **N'hésitez pas venir nous rendre visite.**"
-                        )
-                        
-                        
-        # print("dbgMsg[05] : ", v_txtData)
+        for v_fileName in t_lstTxtFile :
+            if v_fileName != v_except :          
+                if v_fileName == ".gitignore" :
+                    v_txtData = (
+                                "## [ .gitignore ]\n\n" +
+                                "## Liste des fichiers et dossiers à ignorer\n\n" +
+                                "#.gitignore\n\n" +
+                                "## [ Dossiers a ignorer ]\n" +
+                                "_3_software/_3-1_test*/\n" +
+                                "_3_software/oldLibVers*/\n" +
+                                "_5_*/\n" +
+                                "_6_*/\n" +
+                                "_7_*/\n" +
+                                "*_v/\n\n" +
+                                "## [ listes des extentions a ignorer ]\n" +
+                                "*.FCStd1\n" +
+                                "*.264\n" +
+                                "*.mkv\n" +
+                                "*.mp4\n" +
+                                "*.json\n\n" +
+                                "## Compiled source\n" +
+                                "*.com\n" +
+                                "*.class\n" +
+                                "*.dll\n" +
+                                "*.exe\n" +
+                                "*.o\n" +
+                                "*.so\n" +
+                                "*.pyc\n\n" +
+                                "## Packages\n" +
+                                "*.7z\n" +
+                                "*.dmg\n" +
+                                "*.gz\n" +
+                                "*.iso\n" +
+                                "*.jar\n" +
+                                "*.rar\n" +
+                                "*.tar\n" +
+                                "*.zip\n\n" +
+                                "## Logs and databases\n" +
+                                "*.log\n" +
+                                "*.sql\n" +
+                                "*.sqlite\n\n" +
+                                "## OS generated files\n" +
+                                ".DS_Store?\n" +
+                                "ehthumbs.db\n" +
+                                "Icon?\n" +
+                                "Thumbs.db\n" +
+                                "*.swp\n" +
+                                ".*.swp\n" +
+                                "*~\n" +
+                                "*.lock\n" +
+                                "*.out\n"
+                                )
+                                
+                if v_fileName == "README.rst" :
+                    v_txtData = (
+                                "=========================\n" +
+                                "saisir le titre du projet\n" +
+                                "=========================\n\n" +
+                                "   :Autheur:          `Poltergeist42 <https://github.com/poltergeist42>`_\n" +
+                                "   :Projet:          \n" +
+                                "   :Licence:          CC BY-NC-SA 4.0\n" +
+                                "   :Liens:            https://creativecommons.org/licenses/by-nc-sa/4.0/ \n\n" +
+                                "------------------------------------------------------------------------------------------\n\n" +
+                                "Description\n" +
+                                "===========\n\n" +
+                                " Saisir ici une breve description du projet"
+                                )
+                                
+                if v_fileName == "VoLAB.rst" :
+                    v_txtData = (
+                                ".. image:: logoVoLAB_200x200.jpg\n" +
+                                "   :align: center\n\n" +
+                                "=====\n" +
+                                "VoLAB\n" +
+                                "=====\n\n" +
+                                "Nous connaître\n" +
+                                "--------------\n\n" +
+                                "   :Le Site Web:       http://www.volab.org \n\n" +
+                                "   :Le Wiki:           http://www.vorobotics.com/wiki/index.php?title=Accueil \n\n" +
+                                "   :GitHub:            https://github.com/volab \n\n" +
+                                "   :Twitter:           https://twitter.com/vorobotics \n\n" +
+                                "   :Faceboock:         https://www.facebook.com/VoLab95/ \n\n" +
+                                "Qui sommes nous ?\n" +
+                                "-----------------\n\n" +
+                                "   Le VoLAB, premier FabLab du Val d'Oise, est un FabLab associatif portée par\n" +
+                                "   l’associassions VoRoBoTics situé à Vauréal (95480).\n\n" +
+                                "   Nous avons pour vocation le partage non marchand des connaissances\n" +
+                                "   et l'échange de compétences. Petits et gros projets se côtoient dans divers domaines comme :\n\n"
+                                "   Le travail du bois, du métal, l'électronique, la programmation, la sérigraphie,\n" +
+                                "   la couture, le scrapbooking et bien d'autres encore.\n\n" +
+                                "   Les échanges dynamiques dans la bonne humeur et le respect mutuel permettent\n" +
+                                "   à chacun d’appendre et de partager à son rythme\n\n" +
+                                "   **N'hésitez pas venir nous rendre visite.**"
+                                )
+
+                # print("dbgMsg[05] : ", v_txtData)
+                v_fileName = "./project/{}".format( v_fileName )
+                try :
+                    i_fileLog = open(v_fileName, 'a')
+                    i_fileLog.write(v_txtData)
+                    i_fileLog.close()
+                except :
+                    i_fileLog = open(v_fileName, 'w')
+                    i_fileLog.write(v_txtData)
+                    i_fileLog.close()
+                    
+            else : pass
             
-        try :
-            i_fileLog = open(v_fileName, 'a')
-            i_fileLog.write(v_txtData)
-            i_fileLog.close()
-        except :
-            i_fileLog = open(v_fileName, 'w')
-            i_fileLog.write(v_txtData)
-            i_fileLog.close()
             
     def f_copyLogo(self) :
         """ copie du logo dans le repertoire de destination """
