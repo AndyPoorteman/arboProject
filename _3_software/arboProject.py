95b0779550b257039591f0da71c59b600c8c96b1
poltergeist42 <lepoltergeist@gmail.com>
2017-10-24 14:15:01 +0200

    correction de typo + ajout de includeMe ds le '.JSON'

--- _3_software/arboProject.py
+++ _3_software/arboProject.py
@@ -9,7 +9,7 @@ Infos
    :Projet:             arboProject
    :Nom du fichier:     arboProject.py
    :Autheur:            `Poltergeist42 <https://github.com/poltergeist42>`_
-   :Version:            20170808
+   :Version:            20171024
 
 ####
 
@@ -415,9 +415,9 @@ class C_Arbo(object) :
                         elif l[:-1] == "# import sys" :
                             tf.write("import sys\n")
                         elif l[:-1] == "# sys.path.insert(0, os.path.abspath('.'))" :
-                            tf.write(f"sys.path.insert(0, os.path.abspath({v_relativPath}))\n")
+                            tf.write(f"sys.path.insert(0, os.path.abspath(\"{v_relativPath}\"))")
                         elif l[:-1] == "exclude_patterns = []" :
-                            tf.write("exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']\n")
+                            tf.write("exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']")
                         else :
                             tf.write(l)
 
