ed6c446cebbf8b328eac87074d7191c9a9f3aab7
poltergeist42 <lepoltergeist@gmail.com>
2018-09-01 16:59:14 +0200

    UP: createFile

--- _3_software/createFile.py
+++ _3_software/createFile.py
@@ -8,7 +8,7 @@ Infos
 
    :Projet:             arboProject
    :Nom du fichier:     createFile.py
-   :Autheur:            `Poltergeist42 <https://github.com/poltergeist42>`_
+   :Auteur:            `Poltergeist42 <https://github.com/poltergeist42>`_
    :Version:            20170808
 
 ####
@@ -18,24 +18,24 @@ Infos
 
 ####
 
-    :dev language:      Python 3.6
-    :framework:         
+    :dev langage:      Python 3.6
+    :Framework:         
     
 ####
 
-Déscriptif
+Descriptif
 ==========
 
-    :Projet:            Ce projet permet de créer une arboressence de dossier et de sous
-                        dossier nécessaires à chaque nouveau projet. Ainsi que l'ensemble
-                        des fichiers qui y son ratachés. Il permet également d'initialiser
+    :Projet:            Ce projet permet de créer une arborescence de dossier et de sous
+                        dossiers nécessaires à chaque nouveau projet. Ainsi que l'ensemble
+                        des fichiers qui y sont rattachés. Il permet également d'initialiser
                         le dépôt Git et l'outil de documentation Sphinx.
                         
     :Fichiers:          createFile.py regroupe le corps de l'ensemble des fichiers qui
                         doivent être créés par défaut avec chaque nouveau projet.
                         
                         **N.B** : Les fichiers qui sont générer ici sont optionnels et
-                        n'empèche pas la création de l'arborescence ou la configuration
+                        n'empêchent pas la création de l'arborescence ou la configuration
                         de Sphinx.
 
 ####
@@ -57,7 +57,7 @@ lexique
 """
 
 
-#################### Taille maximum des commentaires (90 caracteres)######################
+#################### Taille maximum des commentaires (90 caractères)######################
 
 def f_createREADME(*args, **kwargs) :
     """ Retourne les informations pour la création du fichiers 'README.rst' """
@@ -71,40 +71,40 @@ def f_createREADME(*args, **kwargs) :
         "======================={}\n".format("="*len(v_projectName))+
         "Informations générales {}\n".format(v_projectName)+
         "======================={}\n\n".format("="*len(v_projectName))+
-        ":Autheur:            `Poltergeist42 <https://github.com/poltergeist42>`_\n"\
+        ":Auteur:            `Poltergeist42 <https://github.com/poltergeist42>`_\n"\
         ":Projet:             {}\n".format(v_projectName)+
         ":dépôt GitHub:       \n"\
         ":documentation:      \n"\
         ":Licence:            CC BY-NC-SA 4.0\n"\
         ":Liens:              https://creativecommons.org/licenses/by-nc-sa/4.0/\n\n"\
-        "Déscription\n"\
+        "Description\n"\
         "===========\n\n"\
-        " Saisir ici une brève déscription du projet\n\n"\
+        " Saisir ici une brève description du projet\n\n"\
         "Arborescence du projet\n"\
         "======================\n\n"\
         "Pour aider à la compréhension de mon organisation,"\
-        " voici un bref déscrptif de l'arborescence de se projet."\
+        " voici un bref descriptif de l'arborescence de ce projet."\
         "Cette arborescence est à reproduire si vous récupérez ce"\
         " dépôt depuis GitHub. ::\n\n"\
-        "openFile               # Dossier racine du projet (non versionner)\n"\
-        "|\n"\
-        "+--project             # (branch master) contient l'ensemble du projet en lui même\n"\
-        "|  |\n"\
-        "|  +--_1_userDoc       # Contien toute la documentation relative au projet\n"\
-        "|  |   |\n"\
-        "|  |   \\--source       # Dossier réunissant les sources utilisées par Sphinx\n"\
-        "|  |\n"\
-        "|  +--_2_modelisation  # contien tous les plans et toutes les modélisations du projet\n"\
-        "|  |\n"\
-        "|  +--_3_software      # Contien toute la partie programmation du projet\n"\
-        "|  |\n"\
-        "|  \\--_4_PCB           # Contient toutes les parties des circuits imprimés (routage,\n"\
-        "|                      # implantation, typon, fichier de perçage, etc\n"\
-        "|\n"\
-        "\\--webDoc              # Dossier racine de la documentation qui doit être publiée\n"\
-        "   |\n"\
-        "   \\--html             # (branch gh-pages) C'est dans se dosier que Sphinx vat\n"\
-        "                       # générer la documentation à publié sur internet\n\n"
+        "    ProjectDir_Name        # Dossier racine du projet (non versionner)\n"\
+        "    |\n"\
+        "    +--project             # (branch master) contient l'ensemble du projet en lui-même\n"\
+        "    |  |\n"\
+        "    |  +--_1_userDoc       # Contiens toute la documentation relative au projet\n"\
+        "    |  |   |\n"\
+        "    |  |   \\--source       # Dossier réunissant les sources utilisées par Sphinx\n"\
+        "    |  |\n"\
+        "    |  +--_2_modelisation  # Contiens tous les plans et toutes les modélisations du projet\n"\
+        "    |  |\n"\
+        "    |  +--_3_software      # Contiens toute la partie programmation du projet\n"\
+        "    |  |\n"\
+        "    |  \\--_4_PCB           # Contient toutes les parties des circuits imprimés (routage,\n"\
+        "    |                      # implantation, typon, fichier de perçage, etc.\n"\
+        "    |\n"\
+        "    \\--webDoc              # Dossier racine de la documentation qui doit être publiée\n"\
+        "       |\n"\
+        "       \\--html             # (branch gh-pages) C'est dans ce dossier que Sphinx vat\n"\
+        "                           # générer la documentation a publié sur internet\n\n"
         )
     return  v_fqfn, v_txtData
 
@@ -126,3 +126,4 @@ def f_createInit(*args, **kwargs) :
     
 
            
+
