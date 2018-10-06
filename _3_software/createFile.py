91e6cf62a08c903c338099f05862bdbe48d4d9af
poltergeist42 <lepoltergeist@gmail.com>
2018-10-06 19:38:37 +0200

    creatFile: reformatage de la présentation de l'arborescence du fichier generer 'README.rst'

--- _3_software/createFile.py
+++ _3_software/createFile.py
@@ -87,29 +87,47 @@ def f_createREADME(*args, **kwargs) :
         "Arborescence du projet\n"\
         "======================\n\n"\
         "Pour aider à la compréhension de mon organisation,"\
-        " voici un bref descriptif de l'arborescence de ce projet."\
+        "voici un bref descriptif de l'arborescence de ce projet."\
         "Cette arborescence est à reproduire si vous récupérez ce"\
-        " dépôt depuis GitHub. ::\n\n"\
+        "dépôt depuis GitHub. ::\n\n"\
         "    ProjectDir_Name        # Dossier racine du projet (non versionner)\n"\
         "    |\n"\
-        "    +--project             # (branch master) contient l'ensemble du projet en lui-même\n"\
+        "    +--project             # (Branch master) contient l'ensemble du projet en lui même\n"\
         "    |  |\n"\
         "    |  +--_1_userDoc       # Contiens toute la documentation relative au projet\n"\
         "    |  |   |\n"\
-        "    |  |   \\--source       # Dossier réunissant les sources utilisées par Sphinx\n"\
+        "    |  |   \--source       # Dossier réunissant les sources utilisées par Sphinx\n"\
         "    |  |\n"\
         "    |  +--_2_modelisation  # Contiens tous les plans et toutes les modélisations du projet\n"\
         "    |  |\n"\
         "    |  +--_3_software      # Contiens toute la partie programmation du projet\n"\
         "    |  |\n"\
-        "    |  \\--_4_PCB           # Contient toutes les parties des circuits imprimés (routage,\n"\
-        "    |                      # implantation, typon, fichier de perçage, etc.\n"\
+        "    |  +--_4_PCB           # Contient toutes les parties des circuits imprimés (routage,\n"\
+        "    |  |                   # Implantation, typon, fichier de perçage, etc.)\n"\
+        "    |  |\n"\
+        "    |  +--_5_techDoc       # Contiens toutes les documentations techniques des différents composants\n"\
+        "    |  |                   # ou éléments qui constituent le projet. Ces éléments sont identifiés\n"\
+        "    |  |                   # par un liens Web dans la documentation. Ce dossier n'est pas 'poussé'\n"\
+        "    |  |                   # dans le dépôt distant (.gitignore).\n"\
+        "    |  |\n"\
+        "    |  +--_6_research      # Regroupe toutes les recherches relatives à l'élaboration ou au\n"\
+        "    |  |                   # développement du projet. Ces éléments sont identifiés\n"\
+        "    |  |                   # par un liens Web dans la documentation. Ce dossier n'est pas 'poussé'\n"\
+        "    |  |                   # dans le dépôt distant (.gitignore).\n"\
+        "    |  |\n"\
+        "    |  \--_7_rushes        # Contient tous les éléments qui seront potentiellement intégrés dans la\n"\
+        "    |                      # doc ou dans le projet. Ce dossier n'est pas 'poussé' dans le dépôt \n"\
+        "    |                      # distant (.gitignore).\n"\
         "    |\n"\
-        "    \\--webDoc              # Dossier racine de la documentation qui doit être publiée\n"\
-        "       |\n"\
-        "       \\--html             # (branch gh-pages) C'est dans ce dossier que Sphinx vat\n"\
-        "                           # générer la documentation a publié sur internet\n\n"
-        )
+        "    +--webDoc              # Dossier racine de la documentation qui doit être publiée\n"\
+        "    |  |\n"\
+        "    |  \--html             # (Branch gh-pages) C'est dans ce dossier que Sphinx va\n"\
+        "    |                      # générer la documentation a publié sur internet\n"\
+        "    |\n"\
+        "    \--trash               # Se dossier sert à retirer des éléments du projet sans les\n"\
+        "                           # supprimé réélement. Ce dossier n'est pas pris en compte par\n"\
+        "                           # GIT ou par GitHub\n"\
+       )
     return  v_fqfn, v_txtData
 
 ####
